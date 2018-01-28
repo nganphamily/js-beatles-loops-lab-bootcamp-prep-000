9cfbd61c98609d5fa04726c3fc42e6958432aec8
NGAN PHAM <nganvpham@gmail.com>
2018-01-28 20:17:05 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for (let i=0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+return array;
+}
+
+function johnLennonFacts(facts){
+  var array = [];
+  let i = 0;
+  while(i < facts.length){
+     array.push(`${facts[i]}` + '!!!');
+     i++;
+
+  }
+  return array;
+}
